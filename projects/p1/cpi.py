e1e471fbbabeec5257664f9ba2385b37e438fa69
Hrafn Loftsson <hrafnl@gmail.com>
2022-08-22 14:33:54 +0000

    Updated solution to p1

--- projects/p1/cpi.py
+++ projects/p1/cpi.py
@@ -6,8 +6,9 @@ years_from_now = int(input("Input years: "))
 
 absolute_change_cpi = current_cpi - year_ago_cpi
 inflation = absolute_change_cpi / year_ago_cpi
+cpi_years_from_now = current_cpi * (1 + inflation) ** years_from_now
 
 print()
 print("Change in CPI:", round(absolute_change_cpi, DECIMAL_DIGITS))
 print("Inflation (%):", round(100 * inflation, DECIMAL_DIGITS))
-print("CPI after", years_from_now, "years:", round(current_cpi * (1 + inflation) ** years_from_now, DECIMAL_DIGITS))
+print("CPI after", years_from_now, "years:", round(cpi_years_from_now, DECIMAL_DIGITS))
