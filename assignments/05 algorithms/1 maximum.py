e12051f547d0c19fe9e09b839011820c9366c6ea
PeturOA <peturolafura@gmail.com>
2022-09-07 09:21:18 +0000

    Improve readability

--- assignments/05 algorithms/1 maximum.py
+++ assignments/05 algorithms/1 maximum.py
@@ -4,5 +4,7 @@ num_int = int(input("Input a number: "))
 while num_int >= 0:
     if num_int > max_int:
         max_int = num_int
+
     num_int = int(input("Input a number: "))
+
 print("The maximum is", max_int)
