f6fd5c18d118a52b623d2e2175ad789eea3cb38b
PeturOA <peturolafura@gmail.com>
2022-10-20 00:44:46 +0000

    Improve readability

--- assignments/13 tile traveller/plus/2_shuffle.py
+++ assignments/13 tile traveller/plus/2_shuffle.py
@@ -2,11 +2,12 @@ def shuffle(original_list: list, start_from_back: bool = False) -> list:
     """Returns a shuffled copy of the supplied list. The original list is not modified.
 
     Let n denote the length of the list. The shuffled order is then as follows:
-    0, n-1, 1, n-2, 2 ... and so forth
+    0, n-1, 1, n-2, 2 ... and so forth.
 
     If start_from_back == True then the shuffled order is:
-    n-1, 0, n-2, 1, n-3 ... and so forth
+    n-1, 0, n-2, 1, n-3 ... and so forth.
     """
+
     result = []
     length = len(original_list)
     for i in range(length):
@@ -14,7 +15,10 @@ def shuffle(original_list: list, start_from_back: bool = False) -> list:
             location = i // 2
         else:
             location = length - 1 - i // 2
+
         if start_from_back:
             location = location * (-1) - 1
+
         result.append(original_list[location])
+
     return result
